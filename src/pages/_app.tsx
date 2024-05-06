9f0789e542496d16d1ed0815de374868db6eb7d4
joshua <iuttn123@gmail.com>
2024-05-06 18:17:36 +0900

    prettier applied

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
-import { AppProps } from 'next/app';
-import MiniDrawer from "@/components/MiniDrawer"; // Next.js에서 제공하는 AppProps 타입을 불러옵니다.
+import {AppProps} from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) { // AppProps를 사용하여 파라미터의 타입을 지정합니다.
+const MyApp = ({Component, pageProps}: AppProps) => {
     return (
         <Component {...pageProps} />
     );

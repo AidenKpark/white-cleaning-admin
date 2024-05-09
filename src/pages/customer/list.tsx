75995ea77aa0eed3666ae12b9e17830714d890c0
AidenKpark <yeneung1301@gmail.com>
2024-05-09 22:20:59 +0900

    Add routing for courier, customer, schedule, and sales pages

---
+++ src/pages/customer/list.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Typography } from '@mui/material';
+import CommonLayout from "@/components/layout/CommonLayout";
+
+const CustomerListPage = () => {
+    return (
+        <CommonLayout>
+            <Typography variant="h4">Customer List</Typography>
+            {/* Customer 목록을 보여주는 추가 컴포넌트 또는 로직 */}
+        </CommonLayout>
+    );
+};
+
+export default CustomerListPage;
---
+++ src/pages/schedule/list.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Typography } from '@mui/material';
+import CommonLayout from "@/components/layout/CommonLayout";
+
+const ScheduleListPage = () => {
+    return (
+        <CommonLayout>
+            <Typography variant="h4">Schedule List</Typography>
+            {/* Schedule 목록을 보여주는 추가 컴포넌트 또는 로직 */}
+        </CommonLayout>
+    );
+};
+
+export default ScheduleListPage;

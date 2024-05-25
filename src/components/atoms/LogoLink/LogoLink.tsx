302a1779b30c6a9a2fdc456c7fdcad9e95f5c75f
joshua <iuttn123@gmail.com>
2024-05-25 20:28:38 +0900

    ..

--- src/components/atoms/LogoLink/LogoLink.tsx
+++ src/components/atoms/LogoLink/LogoLink.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
 import COLORS from "@/styles/colors";
@@ -9,6 +9,31 @@ type LogoLinkProps = {
     src: string;
     alt: string;
 }
+
+const a = Array(10).fill(1).map((v,i)=> i+1);
+
+
+const x = Array(10).fill(10).reduce((a,c)=>a+c);
+
+
+
+
+
+
+
+
+
+
+const c = [{name:"americano",price:2500},{name:"latte",price:3500}].map((v)=>{
+    v.name = v.name.toUpperCase();
+    v.price = v.price > 3000 ? v.price + 1000 : v.price - 1000;
+    return v
+})
+
+const d = ["are","you","happy","now"].filter((v)=> !v.includes("a"))
+
+const e = !!""
+
 
 const LogoLink: React.FC<LogoLinkProps> = ({href, src, alt}) => {
     return (
